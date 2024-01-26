7558d071ca05d85321a7e32da1225e5eb2760809
Nareman <naremansharkan5@gmail.com>
2024-01-27 00:23:26 +0200

    get and display meals category

---
+++ app/src/main/java/com/example/foodplanner/model/repo/category/remote/CategoryRemoteDataSourceImp.java
@@ -0,0 +1,40 @@
+package com.example.foodplanner.model.repo.category.remote;
+
+import com.example.foodplanner.api.ApiManager;
+import com.example.foodplanner.api.CategoryCallback;
+import com.example.foodplanner.api.WebService;
+import com.example.foodplanner.model.CategoryResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CategoryRemoteDataSourceImp implements CategoryRemoteDataSource{
+    WebService webService;
+
+    public CategoryRemoteDataSourceImp() {
+        this.webService = ApiManager.getApi();
+    }
+
+    @Override
+    public CategoryResponse.CategoriesItem getCategories(CategoryCallback categoryCallback) {
+        webService.getCategories().enqueue(new Callback<CategoryResponse>() {
+            @Override
+            public void onResponse(Call<CategoryResponse> call, Response<CategoryResponse> response) {
+                if(response.isSuccessful()){
+                    assert response.body() != null;
+                    categoryCallback.onSuccess(response.body().getCategories());
+                }
+                else{
+                    categoryCallback.onCategoryFailure("Something went error");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CategoryResponse> call, Throwable t) {
+                categoryCallback.onCategoryFailure(t.getLocalizedMessage());
+            }
+        });
+        return null;
+    }
+}
--- app/src/main/java/com/example/foodplanner/view/home/HomeView.java
+++ app/src/main/java/com/example/foodplanner/view/home/HomeView.java
@@ -1,8 +1,13 @@
 package com.example.foodplanner.view.home;
 
+import com.example.foodplanner.model.CategoryResponse;
 import com.example.foodplanner.model.RandomMealResponse;
+
+import java.util.List;
 
 public interface HomeView {
     void showSuccessMessage(RandomMealResponse.MealsItem mealsItem);
     void showErrorMessage(String error);
+    void showCategorySuccessMessage(List<CategoryResponse.CategoriesItem> categoriesItems);
+    void showCategoryErrorMessage(String error);
 }
